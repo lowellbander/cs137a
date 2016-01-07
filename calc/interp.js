8b2d0d7d9d2a14cff1cc74f0aec490adaa3354b4
Lowell Bander <lowellbander@gmail.com>
2016-01-07 10:36:39 -0800

    basic operations

--- calc/interp.js
+++ calc/interp.js
@@ -1,6 +1,28 @@
 'use strict';
 
 function interp(ast) {
-  throw new TODO("You're supposed to write your own interpreter!");
+  var expression = ast.es[0];
+  var result = expression.evaluate();
+  return result;
+}
+
+Num.prototype.evaluate = function() {
+  return this.n;
+}
+
+Add.prototype.evaluate = function() {
+  return this.e1.evaluate() + this.e2.evaluate();
+}
+
+Sub.prototype.evaluate = function() {
+  return this.e1.evaluate() - this.e2.evaluate();
+}
+
+Mul.prototype.evaluate = function() {
+  return this.e1.evaluate() * this.e2.evaluate();
+}
+
+Div.prototype.evaluate = function() {
+  return this.e1.evaluate() / this.e2.evaluate();
 }
 
