df750b0d302eed8755df98ad173b1709b402d28e
Lowell Bander <lowellbander@gmail.com>
2016-01-07 10:52:12 -0800

    finished calc

--- calc/interp.js
+++ calc/interp.js
@@ -1,10 +1,13 @@
 'use strict';
 
+var env = {};
+
 function interp(ast) {
   var result;
   for (var i = 0; i < ast.es.length; ++i) {
     result = ast.es[i].evaluate();
   }
+  env = {};
   return result;
 }
 
@@ -30,5 +33,17 @@ Div.prototype.evaluate = function() {
 
 Pow.prototype.evaluate = function() {
   return Math.pow(this.e1.evaluate(), this.e2.evaluate());
+}
+
+Ref.prototype.evaluate = function() {
+  if (this.x in env) {
+    return env[this.x];
+  } else {
+    return 0;
+  }
+}
+
+Assign.prototype.evaluate = function() {
+  return env[this.x] = this.e.evaluate();
 }
 
