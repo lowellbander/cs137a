6c8d655722f70eec970d16062ec879ded0935635
Lowell Bander <lowellbander@gmail.com>
2016-01-15 13:29:27 -0800

    simple let works

--- func/interp.js
+++ func/interp.js
@@ -4,6 +4,25 @@ function interp(ast) {
   return ast.evaluate();
 }
 
+// env is a stack of assignments
+var env = [];
+
+Let.prototype.evaluate = function() {
+  var frame = {};
+  frame[this.x] = this.e1.evaluate();
+  env.push(frame);
+  return this.e2.evaluate();
+}
+
+Var.prototype.evaluate = function() {
+  // find the value in the stack and return it;
+  for (var i = env.length - 1; i >= 0; --i) {
+    if (this.x in env[i]) {
+      return env[i][this.x];
+    }
+  }
+}
+
 Val.prototype.evaluate = function() {
   return this.primValue;
 }
