d406116e30a165afe697fb1067d7615b4c63410b
Lowell Bander <lowellbander@gmail.com>
2016-02-20 13:02:06 -0800

    passes simple method decl, new, and send

--- oo1/trans.js
+++ oo1/trans.js
@@ -1,12 +1,12 @@
 'use strict';
 
 function trans(ast) {
-  debugger;
   return ast.trans();
 }
 
 Program.prototype.trans = function() {
-  var statements = this.ss.map(statement => statement.trans()).join('\n');
+  var statements = "function Obj() {};\n"
+  statements += this.ss.map(statement => statement.trans()).join('\n');
 
   function last(arr) {
     if (!(arr instanceof Array)) throw "last expects an array";
@@ -36,5 +36,23 @@ VarDecl.prototype.trans = function() {
 
 Var.prototype.trans = function() {
   return this.x;
+}
+
+MethodDecl.prototype.trans = function() {
+  return this.C + ".prototype." + this.m + " = (" + this.xs.join(", ")
+    + ") => {" + this.ss.map(s => s.trans()).join("\n") + "};";
+}
+
+Return.prototype.trans = function() {
+  return "return " + this.e.trans() + ";";
+}
+
+New.prototype.trans = function() {
+  return "(new " + this.C + "(" + this.es.map(e => e.trans()).join(", ") + "))";
+}
+
+Send.prototype.trans = function() {
+  return this.erecv.trans() + "." + this.m
+    + "(" + this.es.map(s=>s.trans()).join(", ") + ")";
 }
 
